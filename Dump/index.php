8546b224629b0acacba774807a415ca79ea0295b
Shubh Gupta <shubhorai12@gmail.com>
2024-03-06 00:35:33 +0530

    home page updated

---
+++ Dump/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+    <title>EnergyInsight</title>
+</head>
+<body>
+    <main>
+    <header class="navbar">
+        <div class="left-nav">EnergyInsight</div>
+        <nav class="right-nav">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <!-- Add more navigation links as needed -->
+        </nav>
+    </header>
+
+    <div class="main-content">
+        <span class="prj-name">EnergyInsight</span>
+        <div class="main-heading">
+            Sustainability Plans
+        </div>
+        <div class="subheading">
+            You Make, We Save.
+        </div>
+        <button class="btn-learn-more">Learn More</button>
+        <div class="arrow-icon">↓</div>
+
+    </div>
+    
+    
+</body>
+</html>
