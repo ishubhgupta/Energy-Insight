d0e30213f80ce038ec51e0b51158fac1979d0b63
Shubh Gupta <shubhorai12@gmail.com>
2024-05-05 23:07:03 +0530

    done with index page content

--- Dump/index.php
+++ Dump/index.php
@@ -15,14 +15,14 @@
     <header class="navbar">
         <div class="left-nav">EnergyInsight</div>
         <nav class="right-nav">
-            <a href="#home">Home</a>
+            <a href="#main-content">Home</a>
             <a href="#about">About</a>
             <a href="#services">Services</a>
             <!-- Add more navigation links as needed -->
         </nav>
     </header>
 
-    <div class="main-content">
+    <div class="main-content" id="main-content">
         <span class="prj-name">EnergyInsight</span>
         <div class="main-heading">
             Sustainability Plans
@@ -39,28 +39,36 @@
 
         <div class="quick-links">
             <a href="#intro" class="links">Quick Introduction</a>
-            <a href="#goal" class="links">Goal</a>
+            
 
 
             <a href="appliancesinfo.php" class="links">Appliances</a>
             <a href="heatloadinfo.php" class="links">Heat & Cool Load</a>
-            <a href="#contact" class="links">Contact</a>
+            <a href="#goal" class="links">Goal</a>
+            <!-- <a href="#contact" class="links">Contact</a> -->
         </div>
     </div>
-    <div class="intro" id="Intro">
-        <span class="ql-heading">About Us</span>
-
+    <div class="intro" id="intro">
+        <span class="ql-heading">Problem Statement</span>
+        
         <div class="about-us">
-            
+            <p>Heating and cooling loads significantly impact the energy efficiency of buildings, presenting a critical challenge in sustainable construction and operation. Addressing the optimal management of these loads is essential to minimize energy consumption, reduce greenhouse gas emissions, and enhance overall building performance.</p>
+            <img src="asset\contact-index.jpg" alt="">
         </div>
     </div>
 
     <div class="goal" id="goal">
         <span class="ql-heading">Our Goal</span>
 
-        <div class="our-goal">
-            
-        </div>
+    <div class="our-goal">
+        <ul>
+            <li>To develop a predictive model capable of accurately forecasting the heating and cooling loads of buildings</li>
+            <li>The model will leverage historical data, building characteristics, weather information, and other relevant variables to make predictions</li>
+            <li>Additionally, the project aims to provide actionable suggestions for optimizing heating and cooling systems based on the predicted loads, thereby improving energy efficiency, reducing operational costs, and enhancing environmental sustainability in buildings.</li>
+            <li>Also, we will be able to find the power consumption that would take place due to the appliances that would be used</li>
+        </ul>
+    </div>
+
     </div>
 
     <div class="contact" id="contact">
